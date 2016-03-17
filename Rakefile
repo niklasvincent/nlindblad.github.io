3c1fdbbb089f7b309f6c078da473c80086c0f714
Niklas Lindblad <niklas@lindblad.info>
2016-03-17 21:40:42 +0000

    Add Nginx configuration

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 require 'html-proofer'
 
 task :test do
+    sh "./_nginx/test-config.sh"
     sh "bundle exec jekyll build"
     HTMLProofer.check_directory("./_site", {:parallel => { :in_processes => 3}, :disable_external => false, :assume_extension => true, :only_4xx => true}).run()
 end
---
+++ _nginx/test-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NGINX_CONFIG_LOG="/tmp/nginx-config-check.log"
+NGINX_CACHE_DIR="${DIR}/../nginx-cache"
+
+mkdir -p ${NGINX_CACHE_DIR}
+
+if [[ ! -f ${NGINX_CACHE_DIR}/nginx ]]; then
+  wget https://s3-eu-west-1.amazonaws.com/deploy.niklaslindblad.se/x86_64/nginx -O ${NGINX_CACHE_DIR}/nginx
+fi
+chmod +x ${NGINX_CACHE_DIR}/nginx
+${NGINX_CACHE_DIR}/nginx -T -c ${DIR}/nginx.conf 2>${NGINX_CONFIG_LOG}
+
+if [[ $(cat $NGINX_CONFIG_LOG | egrep "syntax is ok" | wc -l) -eq 1 ]]; then
+  echo "Nginx configuration OK!"
+  exit 0
+else
+  echo -e "Nginx configuration invalid:\n"
+  cat ${NGINX_CONFIG_LOG} | egrep -v "directive makes sense only if the master process runs with super-user privileges" | egrep -v "could not open error log file"
+  exit 1
+fi
