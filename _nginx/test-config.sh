2a39cceca03803dae77248d80b4790cde14368b0
Niklas Lindblad <niklas@lindblad.info>
2016-09-30 17:45:24 +0100

    Use Debian package from S3 instead

--- _nginx/test-config.sh
+++ _nginx/test-config.sh
@@ -5,7 +5,7 @@ NGINX_CONFIG="${DIR}/output/nginx.conf"
 NGINX_CONFIG_LOG="/tmp/nginx-config-check.log"
 NGINX_CACHE_DIR="${DIR}/../nginx-cache"
 NGINX_BINARY="${NGINX_CACHE_DIR}/nginx"
-NGINX_DEB_URL="https://launchpad.net/~ondrej/+archive/ubuntu/nginx-mainline/+build/10448355/+files/nginx-light_1.11.2-1~exp1+deb.sury.org~trusty+3_amd64.deb"
+NGINX_DEB_URL="https://s3-eu-west-1.amazonaws.com/deploy-niklaslindblad-se/nginx/nginx_1.10.0-1-trusty_amd64.deb"
 
 # Download nginx binary, if necessary
 mkdir -p ${NGINX_CACHE_DIR}
